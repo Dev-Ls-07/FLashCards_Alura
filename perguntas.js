3b1c5fe2295ea09171cf2c847d50bf9aa5ba12ea
Luis_Henrique <166053318+Dev-Ls-07@users.noreply.github.com>
2024-11-23 11:31:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao (
+    'Programação', 
+    'O que é JavaScript?', 
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao (
+    'Programção', 
+    'O que é CSS?', 
+    'O CSS é uma linguagem de estilização.'
+)
+
+criaCartao (
+    'Programação', 
+    'O que é uma função?', 
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+
+criaCartao (
+    'Programação',
+    'O que é Java?',
+    'Java é uma linguagem de programação.'
+)
